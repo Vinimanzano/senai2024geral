387d215d79e4a8b95a28105bd5bfa2b313ee6974
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2024-01-29 10:14:55 -0300

    SENAI2024

--- pwbe - web back-end/PROJETO BD/banco/script.sql
+++ pwbe - web back-end/PROJETO BD/banco/script.sql
@@ -5,23 +5,23 @@ use Spotify_da_Shopee;
 create table Musicas(
     id integer primary key auto_increment,
     artista varchar(50) not null,
-    musica varchar(50) not null,
-    album date not null
+    musica varchar(100) not null,
+    album varchar(100) not null
 );
-describe Musicas;
+describe musicas;
 
 -- DML - Popular com dados de teste
 insert into Musicas(artista, musica, album)
 values
-("Drake","","Virginia Beach","For All the Dogs"),
-("Drake","","Calling For You","For All the Dogs"),
-("Drake","","Slime You Out","For All the Dogs"),
-("Drake","","Wick Man","For All the Dogs Scary Hours Edition"),
-("Drake","","God's plan","Scorpion"),
-("Drake","","Hotline Bling","Views"),
-("Drake","","Laugh Now Cry Later","Laugh Now Cry Later"),
-("Drake","","Life Is Good","High Off Life"),
-("Drake","","Another Late Night","For All the Dogs"),
-("Drake","","Headlines","Take Care")
+("Drake","Virginia Beach","For All the Dogs"),
+("Drake","Calling For You","For All the Dogs"),
+("Drake","Slime You Out","For All the Dogs"),
+("Drake","Wick Man","For All the Dogs Scary Hours Edition"),
+("Drake","God's plan","Scorpion"),
+("Drake","Hotline Bling","Views"),
+("Drake","Laugh Now Cry Later","Laugh Now Cry Later"),
+("Drake","Life Is Good","High Off Life"),
+("Drake","Another Late Night","For All the Dogs"),
+("Drake","Headlines","Take Care");
 
-select * from Musicas;""
+select * from Musicas;
