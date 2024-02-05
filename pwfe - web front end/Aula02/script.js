347c2b17fd3914ad3f169ea25b43d69b379b4dbe
vinimanzano <vini.manzano5@gmail.com>
2024-02-05 12:14:07 -0300

    SENAI2024

--- pwfe - web front end/Aula02/script.js
+++ pwfe - web front end/Aula02/script.js
@@ -1,34 +1,27 @@
 function pesquisacep(cep) {
-    // Verify if the browser supports the getJSON method
     if (!jQuery.getJSON) {
         alert("Seu navegador não suporta a função getJSON do jQuery.");
         return;
     }
 
-    // Clear the form fields
     limpa_formulário_cep();
 
-    // Validate the CEP
     cep = cep.replace(/\D/g, '');
     if (cep != "") {
-        // Display "..." while the request is being made
         document.getElementById('logradouro').value = "...";
         document.getElementById('bairro').value = "...";
         document.getElementById('cidade').value = "...";
         document.getElementById('uf').value = "...";
         document.getElementById('ibge').value = "...";
 
-        // Make the request to the ViaCEP webservice
         jQuery.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
             if (!("erro" in dados)) {
-                // If the request is successful, update the form fields with the response data
                 document.getElementById('logradouro').value = dados.logradouro;
                 document.getElementById('bairro').value = dados.bairro;
                 document.getElementById('cidade').value = dados.localidade;
                 document.getElementById('uf').value = dados.uf;
                 document.getElementById('ibge').value = dados.ibge;
             } else {
-                // If the request fails, display an error message
                 limpa_formulário_cep();
                 alert("CEP não encontrado.");
             }
@@ -37,7 +30,6 @@ function pesquisacep(cep) {
 }
 
 function limpa_formulário_cep() {
-    // Clear the form fields
     document.getElementById('cep').value = "";
     document.getElementById('logradouro').value = "";
     document.getElementById('bairro').value = "";
@@ -45,3 +37,4 @@ function limpa_formulário_cep() {
     document.getElementById('uf').value = "";
     document.getElementById('ibge').value = "";
 }
+
