501ab0ad90250c82ab8bd26fb7b168a84388014a
vinimanzano <vini.manzano5@gmail.com>
2024-02-06 08:24:54 -0300

    SENAI2024

--- pwfe - web front end/Aula02/script.js
+++ pwfe - web front end/Aula02/script.js
@@ -1,40 +1,18 @@
 function pesquisacep(cep) {
-    if (!jQuery.getJSON) {
-        alert("Seu navegador não suporta a função getJSON do jQuery.");
-        return;
-    }
-
-    limpa_formulário_cep();
-
-    cep = cep.replace(/\D/g, '');
-    if (cep != "") {
-        document.getElementById('logradouro').value = "...";
-        document.getElementById('bairro').value = "...";
-        document.getElementById('cidade').value = "...";
-        document.getElementById('uf').value = "...";
-        document.getElementById('ibge').value = "...";
-
-        jQuery.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
-            if (!("erro" in dados)) {
-                document.getElementById('logradouro').value = dados.logradouro;
-                document.getElementById('bairro').value = dados.bairro;
-                document.getElementById('cidade').value = dados.localidade;
-                document.getElementById('uf').value = dados.uf;
-                document.getElementById('ibge').value = dados.ibge;
+    $.ajax({
+        url: 'https://viacep.com.br/ws/' + cep + '/json/',
+        type: 'GET',
+        dataType: 'jsonp',
+        success: function(data) {
+            if (!data.erro) {
+                $("#logradouro").val(data.logradouro);
+                $("#bairro").val(data.bairro);
+                $("#cidade").val(data.localidade);
+                $("#uf").val(data.uf);
+                $("#ibge").val(data.ibge);
             } else {
-                limpa_formulário_cep();
                 alert("CEP não encontrado.");
             }
-        });
-    }
+        }
+    });
 }
-
-function limpa_formulário_cep() {
-    document.getElementById('cep').value = "";
-    document.getElementById('logradouro').value = "";
-    document.getElementById('bairro').value = "";
-    document.getElementById('cidade').value = "";
-    document.getElementById('uf').value = "";
-    document.getElementById('ibge').value = "";
-}
-
