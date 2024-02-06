7997d2f11b199fc7efe727ae8d946d53574c40f1
vinimanzano <vini.manzano5@gmail.com>
2024-02-06 08:02:49 -0300

    SENAI2024

--- pwfe - web front end/Aula01/Previsao do tempo/script.js
+++ pwfe - web front end/Aula01/Previsao do tempo/script.js
@@ -25,6 +25,36 @@ async function search() {
 
     city.value = ''
 
+    switch (resultado.weather[0].description) {
+        case 'céu limpo':
+            document.body.style.backgroundImage = 'url(https://source.unsplash.com/1600x900/?clear)'
+            break
+        
+        case 'nublado':
+            document.body.style.backgroundImage = 'url(https://source.unsplash.com/1600x900/?cloud)'
+            break
+
+        case 'chuva':
+            document.body.style.backgroundImage = 'url(https://source.unsplash.com/1600x900/?rain)'
+            break
+
+        case 'névoa':
+            console.log('Haze')
+            document.body.style.backgroundImage = 'url(https://source.unsplash.com/1600x900/?haze)'
+            break
+
+        case 'neve':
+            document.body.style.backgroundImage = 'url(https://source.unsplash.com/1600x900/?snow)'
+            break
+
+        case 'tempestade':
+            document.body.style.backgroundImage = 'url(https://source.unsplash.com/1600x900/?storm)'
+            break
+
+        default:
+            break
+    }
+
     resultado.main.temp = (resultado.main.temp).toFixed(0)
 
     cityName.innerText = resultado.name
