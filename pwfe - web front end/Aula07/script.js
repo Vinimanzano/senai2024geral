356204c37f88e5d11578785b45229f398cd47b0e
vinimanzano <vini.manzano5@gmail.com>
2024-03-07 10:45:39 -0300

    SENAI2024

--- pwfe - web front end/Aula07/script.js
+++ pwfe - web front end/Aula07/script.js
@@ -1,36 +1,11 @@
-const switchModal = () => {
-    const modal = document.querySelector('.modal')
-    const actualStyle = modal.style.display
-    if(actualStyle == 'none') {
-      modal.style.display = 'none'
-    }
-    else {
-      modal.style.display = 'none'
-    }
-  }
-  
-  const btn = document.querySelector('.modalBtn')
-  btn.addEventListener('click', switchModal)
-  
-  window.onclick = function(event) {
-      const modal = document.querySelector('.modal')
-    if (event.target == modal) {
-      switchModal()
-    }
-  }
+const modal = document.querySelector('dialog');
 
-  const bttn = document.querySelector('.btnn')
-  let i=0;
-  bttn.addEventListener('click', () => {
-    const name = document.querySelector('#name').value
-    const email = document.querySelector('#email').value
-    const message = document.querySelector('#message').value
-    alert(`Nome: ${name}\nEmail: ${email}\nMensagem: ${message}`)
-    i++
-  })
+function switchModal() {
+  modal.showModal();
+}
 
-  bttn.addEventListener('click', () => {
-    if(i>0){
-      switchModal()
-    }
-  })
+function closeModal() {
+  modal.close();
+}
+
+
