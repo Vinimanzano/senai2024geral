740ed83e6bbb38e085893c5425045187b21a01eb
vinimanzano <vini.manzano5@gmail.com>
2024-03-14 10:25:01 -0300

    SENAI2024

---
+++ bcd - banco de dados/Projeto - BancoDados/Atividade/api/src/routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const routes = express.Router();
+
+const apiResponse = (req, res) => {
+    res.send('API Online');
+};
+
+const tarefas = require('./controllers/tarefas');
+const usuarios = require('./controllers/usuarios');
+
+routes.get('/', apiResponse);
+
+routes.post('/tarefas', tarefas.create);
+routes.get('/tarefas', tarefas.read);
+routes.put('/tarefas/:id', tarefas.update);
+routes.delete('/tarefas/:id', tarefas.del);
+
+routes.post('/usuarios', usuarios.create);
+routes.get('/usuarios', usuarios.read);
+routes.put('/usuarios/:id', usuarios.update);
+routes.delete('/usuarios/:id', usuarios.del);
+
+module.exports = routes;
+
+
---
+++ bcd - banco de dados/Projeto - BancoDados/Atividade/db/script.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS db;
+CREATE DATABASE db CHARSET=UTF8 COLLATE utf8_general_ci;
+USE db;
+
+CREATE TABLE usuarios(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    senha  VARCHAR (50) NOT NULL
+);
+
+CREATE TABLE tarefas(
+    id  INTEGER PRIMARY KEY AUTO_INCREMENT,
+    descricao TEXT NOT NULL,
+    data_limite DATETIME NOT NULL,
+    status ENUM('Aberto', 'Em Andamento', 'Finalizado') NOT NULL,
+    responsavel INTEGER REFERENCES usuarios(id)
+);
+
+INSERT INTO usuarios (nome, email, senha)
+VALUES 
+    ("Colaborador 1", "colaborador1@email.com", "987654321"),
+    ("Colaborador 2", "colaborador2@email.com", "123456789"),
+    ("Colaborador 3", "colaborador3@email.com", "123454321");
+
+
+INSERT INTO tarefas (descricao, data_limite, status, responsavel)
+VALUES 
+    ("Tarefa de teste 1", NOW() + INTERVAL 1 MONTH, "Aberto", 1),
+    ("Tarefa de teste 2", NOW() + INTERVAL 2 WEEK, "Em Andamento", 2),
+    ("Tarefa de teste 3", NOW() + INTERVAL 3 DAY, "Finalizado", 3);
+
+SELECT * FROM tarefas;
+SELECT * FROM usuarios;
+
+show tables;  
