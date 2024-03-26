bae93c5c096baa480b3b8645d35fc08df6faf662
vinimanzano <vini.manzano5@gmail.com>
2024-03-26 11:27:25 -0300

    att

--- bcd - banco de dados/Aula07 - ATV/script.sql
+++ bcd - banco de dados/Aula07 - ATV/script.sql
@@ -2,99 +2,72 @@ DROP DATABASE IF EXISTS Veiculos;
 CREATE DATABASE Veiculos CHARSET=UTF8 COLLATE utf8_general_ci;
 USE Veiculos;
 
-CREATE TABLE carro(
+CREATE TABLE carro (
     placa VARCHAR(20) PRIMARY KEY NOT NULL,
     modelo VARCHAR(20) NOT NULL,
     marca VARCHAR(50) NOT NULL,
     ano VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE manutencao(
+CREATE TABLE manutencao (
     id_manutencao INTEGER PRIMARY KEY AUTO_INCREMENT,
     inicio_manutencao DATE NOT NULL,
     fim_manutencao DATE NOT NULL,
-    descricao TEXT NOT NULL
+    descricao TEXT NOT NULL,
+    placa VARCHAR(20) NOT NULL,
+    FOREIGN KEY (placa) REFERENCES carro(placa) ON DELETE CASCADE
 );
 
-CREATE TABLE funcionario(
-    matricula INTEGER PRIMARY KEY NOT NULL,
+CREATE TABLE funcionario (
+    matricula VARCHAR(20) PRIMARY KEY NOT NULL,
     funcionario VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE telefone (
-    matricula_funcionario VARCHAR(50) NOT NULL REFERENCES, 
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    matricula_funcionario VARCHAR(20) NOT NULL,
     telefone VARCHAR(30) NOT NULL,
-    telefone_secundario VARCHAR(30)
-)
+    telefone_secundario VARCHAR(30),
+    FOREIGN KEY (matricula_funcionario) REFERENCES funcionario(matricula) ON DELETE CASCADE
+);
 
-
-INSERT INTO carro (placa, modelo, marca, ano)
-VALUES
-("CBA-4403", "Sandeiro", "renaut", "2012"),
+INSERT INTO carro (placa, modelo, marca, ano) VALUES
+("CBA-4403", "Sandero", "Renault", "2012"),
 ("CBC-4945", "Fiorino", "Fiat", "2007"),
-("BEE-7735", "Saveiro", "VW", "2015"),
 ("BEE-7735", "Saveiro", "VW", "2015"),
 ("BBC-8504", "Palio", "Fiat", "2004"),
 ("DEA-7981", "Uno", "Fiat", "2005"),
 ("BEB-5885", "Gol", "VW", "2013"),
-("EDB-2475", "Ranger", "Ford", "2005"),
-("EDB-2475", "Ranger", "Ford", "2005"),
-("BEB-5885", "Gol", "VW", "2013"),
-("CBA-4403", "Sandeiro", "Renaut", "2012"),
-("BBC-8504", "Palio", "Fiat", "2004"),
-("CBC-4945", "Fiorino", "Fiat", "2007"),
-("DEA-7981", "Uno", "Fiat", "2005");
+("EDB-2475", "Ranger", "Ford", "2005");
 
-INSERT INTO manutencao (inicio_manutencao, fim_manutencao, descricao)
-VALUES
-("24/08/2023", "31/08/2023", "Pane elétrica"),
-("13/03/2023", "21/03/2023", "Farol queimado"),
-("30/08/2023", "04/09/2023", "Troca de cavalos por poneis"),
-("29/03/2023", "05/04/2023", "Troca de pneus dianteiros"),
-("27/08/2023", "04/09/2023", "Rebimboca da parafuzeta"),
-("25/02/2023", "04/03/2023", "Lanterna queimada"),
-("16/05/2023", "25/05/2023", "Troca de pneus trazeiros"),
-("05/06/2023", "10/06/2023", "Retrovizor quebrado"),
-("15/07/2023", "19/07/2023", "Troca de Flúido de Freio"),
-("02/09/2023", "07/09/2023", "Lanterna queimada"),
-("14/04/2023", "24/04/2023", "Troca de pneus dianteiros"),
-("30/04/2023", "07/05/2023", "Farol queimado"),
-("25/06/2023", "02/07/2023", "Troca de óleo e revisão geral"),
-("04/08/2023", "10/08/2023", "Problemas no cabo do acelerador");
+INSERT INTO manutencao (inicio_manutencao, fim_manutencao, descricao, placa) VALUES
+("2023-08-24", "2023-08-31", "Pane elétrica", "CBA-4403"),
+("2023-03-13", "2023-03-21", "Farol queimado", "CBC-4945"),
+("2023-08-30", "2023-09-04", "Troca de cavalos por poneis", "BEE-7735"),
+("2023-03-29", "2023-04-05", "Troca de pneus dianteiros", "BEE-7735"),
+("2023-08-27", "2023-09-04", "Rebimboca da parafuzeta", "BBC-8504"),
+("2023-02-25", "2023-03-04", "Lanterna queimada", "DEA-7981"),
+("2023-05-16", "2023-05-25", "Troca de pneus traseiros", "BEB-5885"),
+("2023-06-05", "2023-06-10", "Retrovisor quebrado", "EDB-2475"),
+("2023-07-15", "2023-07-19", "Troca de Flúido de Freio", "EDB-2475"),
+("2023-09-02", "2023-09-07", "Lanterna queimada", "BEB-5885"),
+("2023-04-14", "2023-04-24", "Troca de pneus dianteiros", "CBA-4403"),
+("2023-04-30", "2023-05-07", "Farol queimado", "BBC-8504"),
+("2023-06-25", "2023-07-02", "Troca de óleo e revisão geral", "CBC-4945"),
+("2023-08-04", "2023-08-10", "Problemas no cabo do acelerador", "DEA-7981");
 
-INSERT INTO funcionario (matricula, funcionario)
-VALUES
+INSERT INTO funcionario (matricula, funcionario) VALUES
 ("48562", "Evandro Silva"),
 ("48542", "Jaqueline Teixeira"),
-("48542", "Jaqueline Teixeira"),
-("48522", "Keli Matos"),
 ("48522", "Keli Matos"),
 ("48482", "Osvaldo Oliveira"),
-("48482", "Osvaldo Oliveira"),
-("48482", "Osvaldo Oliveira"),
-("48482", "Osvaldo Oliveira"),
-("48482", "Osvaldo Oliveira"),
-("48502", "Ursula Souza"),
-("48502", "Ursula Souza"),
-("48502", "Ursula Souza"),
 ("48502", "Ursula Souza");
 
-INSERT INTO telefone (matricula_funcionario, telefone, telefone_secundario)
-VALUES
+INSERT INTO telefone (matricula_funcionario, telefone, telefone_secundario) VALUES
 ("48562", "19-53315-2734"),
 ("48542", "19-23003-4864"),
-("48542", "19-23003-4864"),
-("48522", "19-06486-6449", "19-53266-7923"),
 ("48522", "19-06486-6449", "19-53266-7923"),
 ("48482", "19-72077-0521", "19-06078-6843"),
-("48482", "19-72077-0521", "19-06078-6843"),
-("48482", "19-72077-0521", "19-06078-6843"),
-("48482", "19-72077-0521", "19-06078-6843"),
-("48482", "19-72077-0521", "19-06078-6843"),
-("48502", "19-72077-0521", "19-06078-6843"),
-("48502", "19-64378-2404"),
-("48502", "19-64378-2404"),
-("48502", "19-64378-2404"),
 ("48502", "19-64378-2404");
 
 SELECT * FROM carro;
@@ -102,4 +75,4 @@ SELECT * FROM manutencao;
 SELECT * FROM funcionario;
 SELECT * FROM telefone;
 
-show tables;
+SHOW TABLES;
